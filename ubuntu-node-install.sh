09b057cdbd25edb2bb5bffb00a8eac195aab4dd5
sandeep-cs-dev <sandeeppatelsgsits@gmail.com>
2020-06-16 20:09:16 +0530

    nodejs setup ubuntu

---
+++ ubuntu-node-install.sh
@@ -0,0 +1,13 @@
+sudo apt-get update && apt-get upgrade -y
+# node js installation 
+sudo apt-get install -y build-essential
+sudo apt-get install curl python-software-properties
+sudo apt-get install apt-transport-https
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo node -v 
+sudo npm -v 
+
+# install nginx
+
+
